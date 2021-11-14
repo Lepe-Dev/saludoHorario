a8c07e6d0359ed7760dc9f7ee5ee71981c8b18f3
LepeRocket <cristoballepegarcia@gmail.com>
2021-11-14 13:26:52 -0300

    refactor

--- main.js
+++ main.js
@@ -2,26 +2,28 @@ window.onload = () => {
     mostrarSaludo()
 }
 
-function mostrarSaludo() {
-    var fecha = new Date();
-    var hora = fecha.getHours();
-    if(hora >= 6 && hora < 12){
-        var texto = "Buenos Días";
-        var complemento = "Es hora de un desayuno rico y nutritivo"
-        var imagen = "img/breakfast.png";
+function getValuesByHour(hora) {
+    if (hora >= 6 && hora < 12) {
+      return { texto: "Buenos Días", complemento: "Es hora de un desayuno rico y nutritivo", imagen: "img/breakfast.png" };
     }
+  
     if (hora >= 12 && hora < 18) {
-        var texto = "Buenas Tardes";
-        var complemento = "Es hora de un buen almuerzo para mantener las energías"
-        var imagen = "img/lunch.png";
+      return { texto: "Buenas Tardes", complemento: "Es hora de un buen almuerzo para mantener las energías", imagen: "img/lunch.png" };
     }
+  
     if (hora >= 18 && hora < 24 || hora >= 0 && hora < 6) {
-        var texto = "Buenas Noches";
-        var complemento = "Por fin es hora de descansar, mañana será un nuevo día"
-        var imagen = "img/bed.png";
-        
+      return { texto: "Buenas Noches", complemento: "Por fin es hora de descansar, mañana será un nuevo día", imagen: "img/bed.png" };
     }
+  
+    return { texto: "", complemento: "", imagen: "" };
+  }
+  
+  function mostrarSaludo() {
+    const fecha = new Date();
+    const hora = fecha.getHours();
+    const { texto, complemento, imagen } = getValuesByHour(hora);
+  
     document.images["tiempo"].src = imagen;
     document.getElementById('txtsaludo').innerHTML = texto;
     document.getElementById('txtcomplemento').innerHTML = complemento;
-}
+  }
